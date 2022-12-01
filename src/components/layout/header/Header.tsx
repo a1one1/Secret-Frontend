e1ab4df66f51e4af0c0a7f873ae6f8b359e3e913
gataev <email>
2022-12-01 15:59:52 +0300

    Добавил роутинг на сайт / header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,5 +1,21 @@
-import styles from "./Footer.module.css";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 export default function Footer(): JSX.Element {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <img alt="dress" />
+          <img alt="nameshop" />
+        </div>
+        <nav>
+          <Link to="/">Главная</Link>
+          <Link to="/shop">Магазин</Link>
+          <Link to="/brand">О бренде</Link>
+          <Link to="/contacts">Контакты</Link>
+        </nav>
+      </div>
+    </header>
+  );
 }
